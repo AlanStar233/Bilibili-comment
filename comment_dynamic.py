7a57d1d5da53bf705aa3119832e180a08a55f27c
Techzjc <admin@g497813927.com>
2021-06-21 04:04:43 +0800

    Feat: Show user list for the comment

--- comment_dynamic.py
+++ comment_dynamic.py
@@ -29,7 +29,7 @@ class Comment(object):
             # API - Only Display Main Replies
             # comment_url = 'https://api.bilibili.com/x/v2/reply?type={}&oid={}&sort={}&ps={}&pn={}'.format(type_value, oid, sort, ps, pn)
             # API - Replies with Part of Sub-replies
-            comment_url = 'https://api.bilibili.com/x/v2/reply/main?type={}&oid={}&mode={}&next={}'.format(type_value, oid, sort, next_reply)
+            comment_url = 'https://api.bilibili.com/x/v2/reply/main?type={}&oid={}&mode={}&ps={}&next={}'.format(type_value, oid, sort, ps, next_reply)
             # Obtain reply for current page
             while True:
                 try:
@@ -62,7 +62,7 @@ class Comment(object):
             #     uid_list.extend(self.get_comment_uid_list(sort, ps, counter + 1))
             next_reply = comment_info['data']['cursor']['next']
             if next_reply != 1:
-                uid_list.extend(self.get_comment_uid_list(sort, counter=counter+1, next_reply=next_reply))
+                uid_list.extend(self.get_comment_uid_list(sort, ps, counter+1, next_reply=next_reply))
             if remove_repeat:
                 uid_list = list(set(uid_list))
             return uid_list
@@ -85,3 +85,118 @@ class Comment(object):
                 raise ValueError
         else:
             return dynamic_detail
+
+    def get_comment_uname_list(self, sort=2, ps=49, counter=1, refresh_interval=5, sleep=1, next_reply=0, remove_repeat=True):
+        # Technique to avoid 412
+        if counter % refresh_interval == 0:
+            time.sleep(sleep)
+        # Detect reply type & oid
+        dynamic_type = str(self.dynamic_detail['data']['card']['desc']['type'])
+        if dynamic_type in dynamic_types['types']:
+            type_data = dynamic_types['types'][dynamic_type]
+            type_oid_path = type_data['path']
+            oid = self.dynamic_detail
+            for k in type_oid_path:
+                oid = oid[k]
+            type_value = type_data['value']
+
+            # API - Only Display Main Replies
+            # comment_url = 'https://api.bilibili.com/x/v2/reply?type={}&oid={}&sort={}&ps={}&pn={}'.format(type_value, oid, sort, ps, pn)
+            # API - Replies with Part of Sub-replies
+            comment_url = 'https://api.bilibili.com/x/v2/reply/main?type={}&oid={}&mode={}&ps={}&next={}'.format(type_value, oid, sort, ps, next_reply)
+            # Obtain reply for current page
+            while True:
+                try:
+                    topic_info_response = self.session.get(comment_url)
+                    break
+                except:
+                    time.sleep(1)
+            comment_info = json.loads(topic_info_response.content.decode())
+            if comment_info['code'] != 0:
+                if comment_info['code'] == -404:
+                    raise ValueError
+                elif comment_info['code'] == -400:
+                    raise ValueError
+                else:
+                    raise ValueError
+            reply_list = comment_info['data']['replies']
+            # Total Count Approach for API - Only Display Main Replies
+            # total_replies = comment_info['data']['page']['count']
+            # Total Count Approach for API - Replies with Part of Sub-replies
+            # total_replies = comment_info['data']['cursor']['all_count']
+            uname_list = []
+            # Avoid error caused by empty replies
+            if reply_list is None:
+                return []
+            # Add UID for each reply
+            for reply in reply_list:
+                uname_list.append(reply['member']['uname'])
+            # Check if there is another page for API - Only Display Main Replies
+            # if total_replies - ps * counter > 0:
+            #     uid_list.extend(self.get_comment_uid_list(sort, ps, counter + 1))
+            next_reply = comment_info['data']['cursor']['next']
+            if next_reply != 1:
+                uname_list.extend(self.get_comment_uname_list(sort, ps, counter + 1, next_reply=next_reply))
+            if remove_repeat:
+                uname_list = list(set(uname_list))
+            return uname_list
+
+    def get_comment_user_list(self, sort=2, ps=49, counter=1, refresh_interval=5, sleep=1, next_reply=0,
+                                   remove_repeat=True):
+        # Technique to avoid 412
+        if counter % refresh_interval == 0:
+            time.sleep(sleep)
+        # Detect reply type & oid
+        dynamic_type = str(self.dynamic_detail['data']['card']['desc']['type'])
+        if dynamic_type in dynamic_types['types']:
+            type_data = dynamic_types['types'][dynamic_type]
+            type_oid_path = type_data['path']
+            oid = self.dynamic_detail
+            for k in type_oid_path:
+                oid = oid[k]
+            type_value = type_data['value']
+
+            # API - Only Display Main Replies
+            # comment_url = 'https://api.bilibili.com/x/v2/reply?type={}&oid={}&sort={}&ps={}&pn={}'.format(type_value, oid, sort, ps, pn)
+            # API - Replies with Part of Sub-replies
+            comment_url = 'https://api.bilibili.com/x/v2/reply/main?type={}&oid={}&mode={}&ps={}&next={}'.format(
+                type_value, oid, sort, ps, next_reply)
+            # Obtain reply for current page
+            while True:
+                try:
+                    topic_info_response = self.session.get(comment_url)
+                    break
+                except:
+                    time.sleep(1)
+            comment_info = json.loads(topic_info_response.content.decode())
+            if comment_info['code'] != 0:
+                if comment_info['code'] == -404:
+                    raise ValueError
+                elif comment_info['code'] == -400:
+                    raise ValueError
+                else:
+                    raise ValueError
+            reply_list = comment_info['data']['replies']
+            # Total Count Approach for API - Only Display Main Replies
+            # total_replies = comment_info['data']['page']['count']
+            # Total Count Approach for API - Replies with Part of Sub-replies
+            # total_replies = comment_info['data']['cursor']['all_count']
+            user_list = ''
+            # Avoid error caused by empty replies
+            if reply_list is None:
+                return ''
+            # Add UID for each reply
+            for reply in reply_list:
+                # user_list += str.format('{{\"username\": \"{}\", \"uid\": {}}}, ', reply['member']['uname'], reply['mid'])
+                user_list += str.format('\"{}\": \"{}\", ', reply['mid'], reply['member']['uname'])
+            # Check if there is another page for API - Only Display Main Replies
+            # if total_replies - ps * counter > 0:
+            #     uid_list.extend(self.get_comment_uid_list(sort, ps, counter + 1))
+            next_reply = comment_info['data']['cursor']['next']
+            if next_reply != 1:
+                user_list += (self.get_comment_user_list(sort, ps, counter + 1, next_reply=next_reply))
+            if counter == 1:
+                # user_list = '[' + user_list[:(len(user_list) - 2)] + ']'
+                user_list = '{' + user_list[:(len(user_list) - 2)] + '}'
+                user_list = json.loads(user_list)
+            return user_list
